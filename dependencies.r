50811b929eaae254295eb19bf8d0e63e972cde09
Alessandro Joshua Zamproni <zmproni@gmail.com>
2023-01-02 15:56:56 +0800

    Add repos

--- dependencies.r
+++ dependencies.r
@@ -2,5 +2,5 @@
 install.packages("tm", repos = "http://cran.us.r-project.org")
 install.packages("Matrix", repos = "http://cran.us.r-project.org")
 install.packages("plyr", repos = "http://cran.us.r-project.org")
-install.packages("tidyverse")
-install.packages("markdown")
+install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+install.packages("markdown", repos = "http://cran.us.r-project.org")
